9277b3d60d5678c690e9a28a517e88a0275211d5
Harsh_Garg <garg.harsh0014@gmail.com>
2023-09-25 18:25:25 +0530

    created the new ui and handled some edge cases

--- index.js
+++ index.js
@@ -1,57 +1,118 @@
-const input = document.querySelector("#input");
-const checkbtn = document.getElementById("check");
-const result = document.getElementById("result");
-const scoreDisplay = document.getElementById("score");
-const again = document.getElementById("again");
-const highScoreDisplay = document.getElementById("high-score");
+const guessInput = document.querySelector(".guess-input");
+const guessForm = document.querySelector(".guess-form");
+const responseMessageBox = document.querySelector(".response-message");
+const playAgainbtn = document.querySelector(".play-again-btn");
+const remainingGuessesBox = document.querySelector(".remaining-guesses");
+const previousGuessesBox = document.querySelector(".previous-guesses");
 
-let myNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highScore = 0;
+let responseMessage = "";
+let randomNumber = generateRandomNumber();
+let isGameOn = true;
+let guessesLeft = 10;
+let previousGuesses = [];
 
-checkbtn.addEventListener("click", function() {
-    const guessedNumber = Number(input.value);
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!isGameOn) {
+        return;
+    }
+
+    const guess = parseInt(guessInput.value);
+
+    if(!isValidGuess(guess)) {
+        showErrorMessage();
+        return;
+    }
+
+    checkGuess(guess);
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+function isValidGuess(guess) {
+    if(isNaN(guess)) {
+        responseMessage = "Please Enter a Valid Number!";
+        return false;
+    } else if(guess < 1 || guess > 100) {
+        responseMessage = "Please Enter a number in range 1 to 100!";
+        return false;
+    }
+    return true;
+}
+
+function showErrorMessage() {
+    responseMessageBox.innerHTML = responseMessage;
+    responseMessageBox.style.color = "red";
+}
+
+function showNormalMessage() {
+    responseMessageBox.innerHTML = responseMessage;
+    responseMessageBox.style.color = "var(--white)";
+}
+
+function showSuccessMessage() {
+    responseMessageBox.innerHTML = responseMessage;
+    responseMessageBox.style.color = "green";
+}
+
+function checkGuess(guess) {
+    if(guess === randomNumber) {
+        responseMessage = "Your guess is correct!";
+        showSuccessMessage();
+        endGame();
+        return;
+    } else if(guess < randomNumber) {
+        responseMessage = "Guess is Too low!";
+    } else {
+        responseMessage = "Guess is Too high!";
+    }
+
+    guessesLeft--;
+    previousGuesses.push(guess);
+    showResponse();
+    guessInput.value = "";
     
-    if(guessedNumber === 0) 
-    {
-        console.log("Enter something!");
+    if(guessesLeft === 0) {
+        responseMessage = "You Lost!";
+        showErrorMessage();
+        endGame();
     }
-    else if(guessedNumber === myNumber) 
-    {
-        document.querySelector(".header-result").textContent = myNumber;
-        result.textContent = "🎉 Correct number..";
-        document.querySelector("body").style.backgroundColor = "green";
-        document.querySelector(".header-result").style.width = "200px";
-        if(score > highScore)
-        {
-            highScore = score;
-            highScoreDisplay.textContent = `🏅 Highscore: ${highScore}`;
-        }
-        
-    }
-    else 
-    {
-        if(score > 1)
-        {
-            result.textContent = guessedNumber > myNumber ? "📉 Too high.." : "📈 Too low..";
-            score--;
-            scoreDisplay.textContent = `💯 Score: ${score}`;
-        }
-        else
-        {
-            result.textContent = "😢 You lost!";
-        }
-    }
-    
-})
+}
 
-again.addEventListener("click", function() {
-    myNumber = Math.trunc(Math.random() * 20) + 1;
-    score = 20;
-    result.textContent = "🏁 Start Gueesing...";
-    scoreDisplay.textContent = `💯 Score: ${score}`;
-    document.querySelector(".header-result").style.width = "100px";
-    document.querySelector("body").style.backgroundColor = "rgb(25 23 23)";
-    document.querySelector(".header-result").textContent = "?";
-    input.value = "";
-})
+function showResponse() {
+    showNormalMessage();
+    remainingGuessesBox.innerHTML = guessesLeft;
+    previousGuessesBox.innerHTML = previousGuesses;
+}
+
+function showPlayAgainBtn() {
+    playAgainbtn.style.display = "inline";
+}
+
+function hidePlayAgainBtn() {
+    playAgainbtn.style.display = "none";
+}
+
+function endGame() {
+    isGameOn = false;
+    showPlayAgainBtn();
+}
+
+function resetGame() {
+    isGameOn = true;
+    guessesLeft = 10;
+    previousGuesses = [];
+    randomNumber = Math.floor(Math.random() * 100 + 1);
+    responseMessage = "";
+    guessInput.value = "";
+    showResponse();
+}
+
+guessForm.addEventListener("submit", handleSubmit);
+playAgainbtn.addEventListener("click", () => {
+    hidePlayAgainBtn();
+    resetGame();
+});
